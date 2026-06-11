edbdcca3617e1ffef1bcae4d82605c3935f7c726
sakifooo <saki.sasin@gmail.com>
2026-06-11 21:58:40 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,7 @@ export default function Home() {
   const logo = PlaceHolderImages.find(img => img.id === "ouskare-logo");
 
   const matches = [
-    { teamA: "المكسيك", teamB: "جنوب أفريقيا", scoreA: 0, scoreB: 0, time: "20:00 ", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/mexic-vs-southafrica" },
-    { teamA: "بوليفيا", teamB: "الجزائر", scoreA: 0, scoreB: 4, time: "21:00", status: "finished", channel: "الجزائرية الرياضية", commentator: "غير معروف", league: "ولي، مباريات ودية دولية", href: "/live/algeria-vs-bolevia" },
-    { teamA: "النمسا", teamB: "غواتيمالا", scoreA: 0, scoreB: 0, time: "21:45", status: "ملغي", channel: "beIN Sports 3 HD", commentator: "جواد بدة", league: "دولي، مباريات ودية دولية", href: "/live/austria-vs-gwatimala" },
-  ];
+    { teamA: "المكسيك", teamB: "جنوب أفريقيا", scoreA: 0, scoreB: 0, time: "20:00 ", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/mexic-vs-southafrica" },
 
   const seoKeywords = [
    "kora","koora","kooora", "kora live", "yalla live", "مباريات اليوم بث مباشر", "بث مباشر كرة قدم",
