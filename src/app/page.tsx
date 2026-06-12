0b7e6cd242b37a2581de3dcab88fc4d6b9ece925
sakifooo <saki.sasin@gmail.com>
2026-06-12 09:37:13 +0100

    Update match score and status for South Korea vs Czechia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const logo = PlaceHolderImages.find(img => img.id === "ouskare-logo");
 
   const matches = [
-    { teamA: "كوريا الجنوبية", teamB: "تشيكيا ", scoreA: 0, scoreB: 0, time: "20:00 ", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/korea-vs-chek" },
+    { teamA: "كوريا الجنوبية", teamB: "تشيكيا ", scoreA: 2, jscoreB: 1, time: "20:00 ", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/korea-vs-chek" },
     { teamA: "كندا", teamB: "البوسنة", scoreA: 0, scoreB: 4, time: "20:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/canada-vs-bosnia" },
 
   ];
