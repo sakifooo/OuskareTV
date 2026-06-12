3b3afdcab8efe87d18a8debb81170622318901c1
sakifooo <saki.sasin@gmail.com>
2026-06-12 22:05:35 +0100

    Modify match data in Home component
    Updated match details and added a new upcoming match.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,9 @@ export default function Home() {
   const logo = PlaceHolderImages.find(img => img.id === "ouskare-logo");
 
   const matches = [
-   { teamA: "كندا", teamB: "البوسنة", scoreA: 0, scoreB: 1, time: "20:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/canada-vs-bosnia" },
-    { teamA: "كوريا الجنوبية", teamB: "تشيكيا ", scoreA: 2, scoreB: 1, time: "20:00 ", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/korea-vs-chek" },
+    { teamA: "الولايات المتحدة ", teamB: "البراغواي ", scoreA: 0, scoreB: 0, time: "2:00 ", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/usa-vs-paraguay" },
+    { teamA: "كندا", teamB: "البوسنة", scoreA: 1, scoreB: 1, time: "20:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/canada-vs-bosnia" },
+
 
   ];
   const seoKeywords = [
