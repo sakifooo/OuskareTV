f78eeace4e9eedd74927e39b53dd430577720585
sakifooo <saki.sasin@gmail.com>
2026-06-13 22:14:01 +0100

    Update iframe source for maroc-vs-brazil

--- src/app/live/[slug]/page.tsx
+++ src/app/live/[slug]/page.tsx
@@ -13,7 +13,7 @@ export default function LiveMatchPage({ params }: { params: Promise<{ slug: stri
   // الحفاظ على كائن الروابط الجديد وتحديثه
   const iframeMapping: Record<string, string> = {
    "qatar-vs-swiss": "https://1.kooralive360.com/albaplayer/bein-sports-hd-1/?serv=0",
-    "maroc-vs-brazil": "https://1.kooralive360.com/albaplayer/bein-sports-hd-1/?serv=0",
+    "maroc-vs-brazil": "https://8.kooralive360.com/albaplayer/bein-sports-hd-2/?serv=1",
     
   };
 
