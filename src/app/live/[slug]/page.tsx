20c13467533fd531ff2acb96b58293277f32cfc5
sakifooo <saki.sasin@gmail.com>
2026-06-14 10:05:53 +0100

    Update iframeMapping with new match URLs

--- src/app/live/[slug]/page.tsx
+++ src/app/live/[slug]/page.tsx
@@ -12,8 +12,8 @@ export default function LiveMatchPage({ params }: { params: Promise<{ slug: stri
 
   // الحفاظ على كائن الروابط الجديد وتحديثه
   const iframeMapping: Record<string, string> = {
-   "scotland-vs-haiti": "https://1.kooralive360.com/albaplayer/bein-sports-hd-1/?serv=0",
-    "turc-vs-australia": "https://8.kooralive360.com/albaplayer/bein-sports-hd-2/?serv=1",
+   "germany-vs-corasaw": "https://1.kooralive360.com/albaplayer/bein-sports-hd-1/?serv=0",
+    "holand-vs-japan": "https://8.kooralive360.com/albaplayer/bein-sports-hd-2/?serv=1",
     
   };
 
