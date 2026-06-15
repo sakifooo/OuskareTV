f0b9439ac1be714d4bc13af512fc414633ef44d8
sakifooo <saki.sasin@gmail.com>
2026-06-15 13:57:17 +0100

    Update favicon icon path in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,13 +53,11 @@ export const metadata: Metadata = {
     title: 'Tinghir TV',
     description: 'تابع مباريات اليوم بث مباشر.',
   },
-};
 
   icons: {
-    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚽</text></svg>',
+    icon: '/favicon.ico',
   },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
