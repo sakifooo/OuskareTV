1553d01135638b315cb7a5b3d63f3e0a397a1fda
sakifooo <saki.sasin@gmail.com>
2026-06-15 14:09:23 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,53 +1,12 @@
+
+import type {Metadata} from 'next';
+import './globals.css';
+
 export const metadata: Metadata = {
-  title: '⚽ Tinghir TV - بث مباشر ونتائج حية',
-
-  description:
-    'تابع عبر Tinghir TV بث مباشر لأهم مباريات اليوم، نتائج المباريات، مواعيد اللقاءات وأخبار الدوريات العالمية مثل الدوري الإسباني والإيطالي والسعودي ودوري أبطال أوروبا.',
-
-  keywords: [
-    'Tinghir TV',
-    'بث مباشر',
-    'مباريات اليوم',
-    'نتائج المباريات',
-    'الدوري الإسباني',
-    'الدوري الإيطالي',
-    'الدوري السعودي',
-    'دوري أبطال أوروبا',
-    'live football',
-  ],
-
-  metadataBase: new URL('https://ouskaretv.vercel.app'),
-
-  verification: {
-    google: 'VzK0Ce6V01Jr_8MvkE_i_rwtYftEtrnNdtl5KY1cIdQ',
-  },
-
-  robots: {
-    index: true,
-    follow: true,
-  },
-
-  alternates: {
-    canonical: 'https://ouskaretv.vercel.app/',
-  },
-
-  openGraph: {
-    title: 'Tinghir TV',
-    description: 'تابع مباريات اليوم بث مباشر.',
-    url: 'https://ouskaretv.vercel.app/',
-    siteName: 'Tinghir TV',
-    locale: 'ar_MA',
-    type: 'website',
-    
-  },
-
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Tinghir TV',
-    description: 'تابع مباريات اليوم بث مباشر.',
-  },
-
-icons: {
+  title: '⚽ Ouskare TV - بث مباشر ونتائج حية',
+  description: 'Ouskare TV live matchs بث مباشر مباريات اليوم اون لاين بدون تقطيع، نتائج المباريات، الدوري الإنجليزي، الدوري الإسباني، الدوري السعودي، دوري أبطال أوروبا.',
+  keywords: 'Ouskare TV, ouskare, بث مباشر, مباريات اليوم, نتائج المباريات, الدوري الانجليزي, الدوري الاسباني, الدوري السعودي, دوري ابطال اوروبا, live stream football',
+  icons: {
     icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚽</text></svg>',
   },
 };
