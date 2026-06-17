d55ddbb4ff5a0867209a892535cda2d625a61a94
sakifooo <saki.sasin@gmail.com>
2026-06-17 12:50:17 +0100

    Replace match URLs in iframeMapping
    Updated iframeMapping with new match URLs.

--- src/app/live/[slug]/page.tsx
+++ src/app/live/[slug]/page.tsx
@@ -12,8 +12,8 @@ export default function LiveMatchPage({ params }: { params: Promise<{ slug: stri
 
   // الحفاظ على كائن الروابط الجديد وتحديثه
   const iframeMapping: Record<string, string> = {
-    "france-vs-senigal": "https://1.kooralive360.com/albaplayer/bein-sports-hd-1/?serv=0",
-    "norway-vs-iraq": "https://1.kooralive360.com/albaplayer/bein-sports-hd-1/?serv=0",
+    "portugal-vs-congo": "https://1.kooralive360.com/albaplayer/bein-sports-hd-1/?serv=0",
+    "england-vs-croitia": "https://1.kooralive360.com/albaplayer/bein-sports-hd-1/?serv=0",
     "alger-vs-argentina": "https://1.kooralive360.com/albaplayer/bein-sports-hd-1/?serv=0",
 
   };
