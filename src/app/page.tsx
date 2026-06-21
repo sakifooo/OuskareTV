98f2370398cf7a1b511c68b2451ef754b7af0141
sakifooo <saki.sasin@gmail.com>
2026-06-21 10:04:47 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ import { PlaceHolderImages } from "@/lib/placeholder-images";
 export default function Home() {
   const logo = PlaceHolderImages.find(img => img.id === "ouskare-logo");
   const matches = [
-    { teamA: "الاكوادور", teamB: "كوراساو ", scoreA: 0, scoreB: 0, time: "21:00", status: "live", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/ecwador-vs-corasaw" },
-    { teamA: "تونس", teamB: "اليابان", scoreA: 0, scoreB: 0, time: "18:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/tunis-vs-japon" },
+    { teamA: "الاكوادور", teamB: "كوراساو ", scoreA: 0, scoreB: 0, time: "21:00", status: "finished", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/ecwador-vs-corasaw" },
+    { teamA: "تونس", teamB: "اليابان", scoreA: 0, scoreB: 4, time: "18:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/tunis-vs-japon" },
   ];
  
   const seoKeywords = [
