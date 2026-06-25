1d01117d56c361dbc015fa611c62fc3f6f993f00
sakifooo <saki.sasin@gmail.com>
2026-06-25 23:23:22 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ import { PlaceHolderImages } from "@/lib/placeholder-images";
 export default function Home() {
   const logo = PlaceHolderImages.find(img => img.id === "ouskare-logo");
   const matches = [
-    { teamA: "الإكوادور", teamB: "ألمانيا", scoreA: 2, scoreB: 1, time: "21:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/ecwador-vs-germany" },
-    { teamA: "كوراساو", teamB: "كوت ديفوار", scoreA: 0, scoreB: 2, time: "21:00", status: "live", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/corasaw-vs-cotedivoire" },
+    { teamA: "الإكوادور", teamB: "ألمانيا", scoreA: 2, scoreB: 1, time: "21:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/ecwador-vs-germany" },
+    { teamA: "كوراساو", teamB: "كوت ديفوار", scoreA: 0, scoreB: 2, time: "21:00", status: "finished", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/corasaw-vs-cotedivoire" },
     { teamA: "اليابان", teamB: "السويد", scoreA: 0, scoreB: 0, time: "00:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/japon-vs-swed" },
     { teamA: "هلندا", teamB: "تونس", scoreA: 0, scoreB: 0, time: "00:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/tunis-vs-holand" },  
   ];
