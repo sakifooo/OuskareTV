a9150c7def062c4aac94e8feb736a1708f77650a
sakifooo <saki.sasin@gmail.com>
2026-06-25 12:24:37 +0100

    Update match list with new upcoming games

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ import { PlaceHolderImages } from "@/lib/placeholder-images";
 export default function Home() {
   const logo = PlaceHolderImages.find(img => img.id === "ouskare-logo");
   const matches = [
-    { teamA: "المكسيك", teamB: "تشيكيا", scoreA: 0, scoreB: 0, time: "02:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/chek-vs-mexic" },
-    { teamA: "كوريا", teamB: "جنوب افريقيا", scoreA: 0, scoreB: 0, time: "02:00", status: "live", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/korea-vs-southafrica" },
-    { teamA: "هايتي", teamB: "المغرب", scoreA: 2, scoreB: 4, time: "23:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/haiti-vs-maroc" },
-    { teamA: "اسكتلندا", teamB: "البرازيل", scoreA: 0, scoreB: 3, time: "23:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/scotland-vs-brazil" },  
+    { teamA: "الإكوادور", teamB: "ألمانيا", scoreA: 0, scoreB: 0, time: "21:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/ecwador-vs-germany" },
+    { teamA: "كوراساو", teamB: "كوت ديفوار", scoreA: 0, scoreB: 0, time: "21:00", status: "upcoming", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/corasaw-vs-cotedivoire" },
+    { teamA: "اليابان", teamB: "السويد", scoreA: 0, scoreB: 0, time: "00:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/japon-vs-swed" },
+    { teamA: "هلندا", teamB: "تونس", scoreA: 0, scoreB: 0, time: "00:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/tunis-vs-holand" },  
   ];
  
   const seoKeywords = [
