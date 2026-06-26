7775f7f9b476451d97bb2318c620beff3a937663
sakifooo <saki.sasin@gmail.com>
2026-06-27 00:13:09 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,10 @@ export default function Home() {
   const matches = [
     { teamA: "النرويج", teamB: "فرنسا", scoreA: 1, scoreB: 4, time: "20:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/norway-vs-france" },
     { teamA: "العراق", teamB: "السنغال", scoreA: 0, scoreB: 5, time: "20:00", status: "finished", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/senigal-vs-iraq" },
-    { teamA: "الرأس الأخضر", teamB: "السعودية", scoreA: 0, scoreB: 0, time: "01:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/capverde-vs-saudi" },
-    { teamA: "الاروغواي", teamB: "اسبانيا", scoreA: 0, scoreB: 0, time: "01:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/orogway-vs-spain" },  
-    { teamA: "مصر ", teamB: "ايران", scoreA: 0, scoreB: 0, time: "04:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/egeypt-vs-iran" },
-    { teamA: "نيوزيلاندا", teamB: "بلجيكا", scoreA: 0, scoreB: 0, time: "04:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/newzland-vs-belgique" },  
+    { teamA: "الرأس الأخضر", teamB: "السعودية", scoreA: 0, scoreB: 0, time: "01:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/capverde-vs-saudi" },
+    { teamA: "الاروغواي", teamB: "اسبانيا", scoreA: 0, scoreB: 0, time: "01:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/orogway-vs-spain" },  
+    { teamA: "مصر ", teamB: "ايران", scoreA: 0, scoreB: 0, time: "04:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/egeypt-vs-iran" },
+    { teamA: "نيوزيلاندا", teamB: "بلجيكا", scoreA: 0, scoreB: 0, time: "04:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/newzland-vs-belgique" },  
 
   ];
  
