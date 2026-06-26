ff0355c2340e15daca56770bac8d7eda697dbe70
sakifooo <saki.sasin@gmail.com>
2026-06-26 22:53:58 +0100

    Update match scores for Norway and Iraq

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ import { PlaceHolderImages } from "@/lib/placeholder-images";
 export default function Home() {
   const logo = PlaceHolderImages.find(img => img.id === "ouskare-logo");
   const matches = [
-    { teamA: "النرويج", teamB: "فرنسا", scoreA: 0, scoreB: 0, time: "20:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/norway-vs-france" },
-    { teamA: "العراق", teamB: "السنغال", scoreA: 0, scoreB: 0, time: "20:00", status: "finished", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/senigal-vs-iraq" },
+    { teamA: "النرويج", teamB: "فرنسا", scoreA: 4, scoreB: 1, time: "20:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/norway-vs-france" },
+    { teamA: "العراق", teamB: "السنغال", scoreA: 0, scoreB: 5, time: "20:00", status: "finished", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/senigal-vs-iraq" },
     { teamA: "الرأس الأخضر", teamB: "السعودية", scoreA: 0, scoreB: 0, time: "01:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/capverde-vs-saudi" },
     { teamA: "الاروغواي", teamB: "اسبانيا", scoreA: 0, scoreB: 0, time: "01:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/orogway-vs-spain" },  
     { teamA: "مصر ", teamB: "ايران", scoreA: 0, scoreB: 0, time: "04:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/egeypt-vs-iran" },
