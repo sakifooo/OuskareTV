19098f2cdb9e58efac36ea32714798c1bdf69261
sakifooo <saki.sasin@gmail.com>
2026-06-29 14:22:55 +0100

    Update match data with upcoming games

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,8 @@ export default function Home() {
   const logo = PlaceHolderImages.find(img => img.id === "ouskare-logo");
   const matches = [
    
-    { teamA: "كندا", teamB: "جنوب افريقيا", scoreA: 1, scoreB: 0, time: "20:00", status: "finished", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/canada-vs-southafrica" },  
-    { teamA: "الجزائر ", teamB: "النمسا", scoreA: 3, scoreB: 3, time: "03:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/alger-vs-austria" },
-    { teamA: "الارجنتين", teamB: "الاردن", scoreA: 3, scoreB: 1, time: "03:00", status: "finished", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/argentin-vs-jordon" },  
+    { teamA: "اليابان", teamB: " البرازيل", scoreA: 0, scoreB: 0, time: "18:00", status: "upcoming", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/canada-vs-southafrica" },  
+    { teamA: "المانيا ", teamB: "البراغواي", scoreA: 0, scoreB: 0, time: "21:30", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/alger-vs-austria" },
 
   ];
  
