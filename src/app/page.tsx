c38c902ef45d83106c2cb60ef39e1ee183e8f60d
sakifooo <saki.sasin@gmail.com>
2026-06-30 00:33:48 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ import { PlaceHolderImages } from "@/lib/placeholder-images";
 export default function Home() {
   const logo = PlaceHolderImages.find(img => img.id === "ouskare-logo");
   const matches = [
-    { teamA: "المغرب", teamB: "هولاندا", scoreA: 0, scoreB: 0, time: "02:00", status: "upcoming", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/maroc-vs-holand" },  
-    { teamA: "المانيا ", teamB: "البراغواي", scoreA: 0, scoreB: 0, time: "21:30", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/germany-vs-paragway" },
+    { teamA: "المغرب", teamB: "هولاندا", scoreA: 0, scoreB: 0, time: "02:00", status: "live", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/maroc-vs-holand" },  
+    { teamA: "المانيا ", teamB: "البراغواي", scoreA: 1, scoreB: 1, time: "21:30", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/germany-vs-paragway" },
     { teamA: "اليابان", teamB: " البرازيل", scoreA: 1, scoreB: 2, time: "18:00", status: "finished", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/japon-vs-brazil" },  
 
   ];
