cb52cf7b4263568805bc2ffd7f1682413064449d
sakifooo <saki.sasin@gmail.com>
2026-06-30 14:16:47 +0100

    Update match data with new teams and scores

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,8 @@ import { PlaceHolderImages } from "@/lib/placeholder-images";
 export default function Home() {
   const logo = PlaceHolderImages.find(img => img.id === "ouskare-logo");
   const matches = [
-    { teamA: "هلندا", teamB: "المغرب", scoreA: 0, scoreB: 0, time: "02:00", status: "live", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/maroc-vs-holand" },  
-    { teamA: "المانيا ", teamB: "البراغواي", scoreA: 1, scoreB: 1, time: "21:30", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/germany-vs-paragway" },
-    { teamA: "اليابان", teamB: " البرازيل", scoreA: 1, scoreB: 2, time: "18:00", status: "finished", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/japon-vs-brazil" },  
+    { teamA: "النرويج", teamB: "ساحل العاج", scoreA: 0, scoreB: 0, time: "18:00", status: "upcoming", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/cotedivoire-vs-norway" },  
+    { teamA: "فرنسا ", teamB: "السويد", scoreA: 0, scoreB: 0, time: "22:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/france-vs-swed" },
 
   ];
  
