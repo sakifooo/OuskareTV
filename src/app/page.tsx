e2a8e7a3b950e7bff37f6794a84ddde8b96ccde8
sakifooo <saki.sasin@gmail.com>
2026-07-04 02:30:18 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
   const logo = PlaceHolderImages.find(img => img.id === "ouskare-logo");
   const matches = [
     { teamA: "مصر", teamB: "استراليا ", scoreA: 1, scoreB: 1, time: "19:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/egypt-vs-australia" }, 
-    { teamA: "رأس الاخضر ", teamB: "الارجنتين", scoreA: 0, scoreB: 0, time: "23:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/capvers-vs-argentina" },
-    { teamA: "غانا ", teamB: "كلومبيا ", scoreA: 0, scoreB: 0, time: "02:30", status: "upcoming", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/gana-vs-colombia" },  
+    { teamA: "رأس الاخضر ", teamB: "الارجنتين", scoreA: 2, scoreB: 3, time: "23:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/capvers-vs-argentina" },
+    { teamA: "غانا ", teamB: "كلومبيا ", scoreA: 0, scoreB: 0, time: "02:30", status: "live", channel: "beIN SPORTS MAX 2", commentator: "غير معروف", league: "كأس العالم", href: "/live/gana-vs-colombia" },  
 
   ];
  
