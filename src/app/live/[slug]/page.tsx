4868fee33295b29f5ca759fefbc2c957ac2f2a67
sakifooo <saki.sasin@gmail.com>
2026-07-06 10:29:35 +0100

    Update page.tsx

--- src/app/live/[slug]/page.tsx
+++ src/app/live/[slug]/page.tsx
@@ -7,12 +7,12 @@ export default function LiveMatchPage({ params }: { params: Promise<{ slug: stri
   const slug = resolvedParams.slug;
 
   const iframeMapping: Record<string, string[]> = {
-    "brazil-vs-norvege": [
+    "portugal-vs-spain": [
       "https://max.mpnh.online/albaplayer/max1/?serv=1", // Server 1
       "https://912acsss8af382.shootny.com/playerv5.php?match=4750009&key=9f39972b67d6ce22189507d008acwc26", // Server 2
     ],
 
-    "mexico-vs-england": [
+    "belgique-vs-usa": [
     "https://max.mpnh.online/albaplayer/max1/?serv=1", // Server 1
       "https://912acsss8af382.shootny.com/playerv5.php?match=4750009&key=9f39972b67d6ce22189507d008acwc26", // Server 2
 
