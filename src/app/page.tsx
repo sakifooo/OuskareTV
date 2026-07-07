a9e23622575b3ec8c166ad09519a5381000f6606
sakifooo <saki.sasin@gmail.com>
2026-07-07 22:01:20 +0100

    Update match status from 'upcoming' to 'live'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const logo = PlaceHolderImages.find(img => img.id === "ouskare-logo");
   const matches = [
     { teamA: "مصر", teamB: "الارجنتين ", scoreA: 2, scoreB: 3, time: "17:00", status: "finished", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/egypt-vs-argentina" }, 
-    { teamA: " سويسرا", teamB: "كلومبيا ", scoreA: 0, scoreB: 0, time: "21:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/swis-vs-colombia" },
+    { teamA: " سويسرا", teamB: "كلومبيا ", scoreA: 0, scoreB: 0, time: "21:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: "كأس العالم", href: "/live/swis-vs-colombia" },
 
   ];
  
