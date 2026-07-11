7ec4ead396bf4ae9f60a8cd3171be830566afb56
sakifooo <saki.sasin@gmail.com>
2026-07-11 16:26:41 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { PlaceHolderImages } from "@/lib/placeholder-images";
 export default function Home() {
   const logo = PlaceHolderImages.find(img => img.id === "ouskare-logo");
   const matches = [
-    { teamA: "اسبانيا", teamB: "بلجيكا ", scoreA: 0, scoreB: 0, time: "20:00", status: "live", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: " ربع النهائي", href: "/live/spain-vs-belgique" }, 
+    { teamA: "النرويج", teamB: "انجلترا ", scoreA: 0, scoreB: 0, time: "22:00", status: "upcoming", channel: "beIN SPORTS MAX 1", commentator: "غير معروف", league: " ربع النهائي", href: "/live/england-vs-norway" }, 
 
   ];
  
